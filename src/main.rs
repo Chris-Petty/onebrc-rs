f31b0e443c2f71a60e95bacf3a19211b420f72d0
Chris Petty <chris@sussol.net>
2024-03-09 12:56:43 +1300

    2nd - via undo lol. ~110MBps

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,13 @@
 // So first did what I understood and just used the iterator and that was ugly. Read all the options for a string
 // And there was split_once() which I figure does nearly the same thing.
 //// ~100MBps, 125s. Same class as golang now!
-// 3rd - Multi threaded crudely (I guess?) like my goroutines. Also stopped co pilot here so I absorbed some docs maybe.
-// So I wasn't getting results I expected. Turns out the loop, doing nothing but checking Ok(line) only pulls ~250MBps
-// Not good, go in similar circumstance did 700MBps :(. Anyway...
-//// ~75MBps, TOO LONG. Maybe copilot will help... maybe. Maybe a pool is better? Better ways to read? Better IPC?
+//
 //
 
 use std::{
-    collections::HashMap,
-    fs,
+    collections, fs,
     io::{BufRead, BufReader},
-    sync::mpsc::{self, Receiver, Sender},
-    thread, time,
+    time,
 };
 
 struct CityAnalysis {
@@ -38,12 +33,12 @@ struct CityAnalysis {
 }
 
 impl CityAnalysis {
-    fn new() -> Self {
+    fn new(temperature: f32) -> Self {
         CityAnalysis {
-            min: f32::MAX,
-            max: f32::MIN,
-            sum: 0.0,
-            count: 0,
+            min: temperature,
+            max: temperature,
+            sum: temperature,
+            count: 1,
         }
     }
 }
@@ -57,60 +52,26 @@ fn main() {
     let file = fs::File::open("../1brc/measurements.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let (tx, rx): (
-        Sender<HashMap<String, CityAnalysis>>,
-        Receiver<HashMap<String, CityAnalysis>>,
-    ) = mpsc::channel();
-
-    let mut chunk: Vec<String> = Vec::new();
-    let mut children = Vec::new();
+    let mut data: collections::HashMap<String, CityAnalysis> = collections::HashMap::new();
 
     for line in reader.lines() {
         if let Ok(line) = line {
-            // Up to here is only 250MBps :'(
-            chunk.push(line);
-            if chunk.len() == 50000 {
-                let thread_chunk = chunk.clone();
-                let thread_tx = tx.clone();
-                let child = thread::spawn(move || {
-                    let mut data: HashMap<String, CityAnalysis> = HashMap::new();
+            let parts = line.split_once(';').unwrap();
+            let city = parts.0;
+            let temperature: f32 = parts.1.parse().unwrap();
+            let entry = data
+                .entry(city.to_string())
+                .or_insert(CityAnalysis::new(temperature));
 
-                    for line in thread_chunk {
-                        let parts = line.split_once(';').unwrap();
-                        let city = parts.0;
-                        let temperature: f32 = parts.1.parse().unwrap();
-                        let entry = data.entry(city.to_string()).or_insert(CityAnalysis::new());
-
-                        entry.min = entry.min.min(temperature);
-                        entry.max = entry.max.max(temperature);
-                        entry.sum += temperature;
-                        entry.count += 1;
-                    }
-                    thread_tx.send(data).unwrap();
-                });
-                children.push(child);
-                chunk.clear();
-            }
+            entry.min = entry.min.min(temperature);
+            entry.max = entry.max.max(temperature);
+            entry.sum += temperature;
+            entry.count += 1;
         }
     }
 
-    let mut data: HashMap<String, CityAnalysis> = HashMap::new();
-    for child in children {
-        child.join().expect("OH NO");
-        let analysis_map = rx.recv().expect("better be a map lol");
-
-        analysis_map.iter().for_each(|(key, city)| {
-            let entry = data.entry(key.to_string()).or_insert(CityAnalysis::new());
-
-            entry.min = entry.min.min(city.min);
-            entry.max = entry.max.max(city.max);
-            entry.sum += city.sum;
-            entry.count += city.count;
-        });
-    }
-
+    let mut output: Vec<String> = vec![];
     // print the results
-    let mut output = Vec::new();
     for (city, analysis) in data {
         // append to the output string
         output.push(format!(
@@ -126,7 +87,7 @@ fn main() {
     let output_text = output.join(", ");
     println!("{{{}}}", output_text);
 
-    // // end timer
+    // end timer
     total_duration += start.elapsed();
     println!("Time: {:.2} ms", total_duration.as_millis());
 }
